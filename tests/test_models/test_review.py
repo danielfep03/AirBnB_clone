3989a3d74d8eb56441dde079e88b64aa9f1daf5f
Dc-cpu-arch <d.garzong77@gmail.com>
2021-02-17 19:48:51 +0000

    Tests Review class

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+	Test Module - Review
+"""
+
+import unittest
+from models.base_model import BaseModel
+from models.review import Review
+import uuid
+import datetime
+
+
+class AttributesTest(unittest.TestCase):
+	"""  Tests place_id user_id and text """
+
+
+	def test_review(self):
+
+		mock = Review()
+		self.asserIsNotNone(mock.place_id)
+		self.asserIsNotNone(mock.user_id)
+		self.asserIsNotNone(mock.text)
+
