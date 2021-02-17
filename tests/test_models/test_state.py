bbc9cad1b60895085ec348fdad9cc5ad63e8b649
Dc-cpu-arch <d.garzong77@gmail.com>
2021-02-17 19:48:32 +0000

    Test state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+	Module Test to ckeck state
+"""
+
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+import uuid
+import datetime
+
+
+class AttributesTest(unittest.TestCase):
+	""" Tests State's name """
+
+
+	def test_state(self):
+		""" tests name """
+
+		mock = State()
+		self.assertIsNotNone(mock.name)
+
